7a8a4dc5ab3ea4de09c536b0ec6750c5f4b80cb4
Carl Ortutay <carlorty@gmail.com>
2019-10-07 12:00:52 -0400

    completed Organizing your React code

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,61 +1,45 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {Component} from "react";
+import {Link, withRouter} from "react-router-dom";
 import {format} from "date-fns";
+import PropTypes from "prop-types";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import ApiContext from "../ApiContext";
-import config from "../config";
 import "./Note.css";
 
-export default class Note extends React.Component {
-  static defaultProps = {
-    onDeleteNote: () => {},
-  };
-  static contextType = ApiContext;
-
-  handleClickDelete = e => {
-    e.preventDefault();
-    const noteId = this.props.id;
-
-    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then(res => {
-        if (!res.ok) return res.json().then(e => Promise.reject(e));
-        return res.json();
-      })
-      .then(() => {
-        this.context.deleteNote(noteId);
-        this.props.onDeleteNote(noteId);
-      })
-      .catch(error => {
-        console.error({error});
-      });
-  };
-
+class Note extends Component {
   render() {
-    const {name, id, modified} = this.props;
     return (
       <div className='Note'>
         <h2 className='Note__title'>
-          <Link to={`/note/${id}`}>{name}</Link>
+          <Link to={`/note/${this.props.id}`}>{this.props.name}</Link>
         </h2>
         <button
           className='Note__delete'
           type='button'
-          onClick={this.handleClickDelete}
+          onClick={() => {
+            this.props.handleDelete(this.props.id);
+            this.props.history.push("/");
+          }}
         >
           <FontAwesomeIcon icon='trash-alt' /> remove
         </button>
         <div className='Note__dates'>
           <div className='Note__dates-modified'>
             Modified{" "}
-            <span className='Date'>{format(new Date(modified), "Do MMM yyyy")}</span>
+            <span className='Date'>
+              {format(new Date(this.props.modified), "Do MMM yyyy")}
+            </span>
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(Note);
+
+Note.propTypes = {
+  modified: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  handleDelete: PropTypes.func,
+};
--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,30 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Note from "../Note/Note";
 import CircleButton from "../CircleButton/CircleButton";
-import ApiContext from "../ApiContext";
-import {getNotesForFolder} from "../notes-helpers";
 import "./NoteListMain.css";
+import NoteContext from "../NoteContext";
+import {getNotesForFolder} from "../notes-helpers";
+import PropTypes from "prop-types";
 
-export default class NoteListMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {},
-    },
-  };
-  static contextType = ApiContext;
-
+export default class NoteListMain extends Component {
+  static contextType = NoteContext;
   render() {
     const {folderId} = this.props.match.params;
-    const {notes = []} = this.context;
-    const notesForFolder = getNotesForFolder(notes, folderId);
     return (
       <section className='NoteListMain'>
         <ul>
-          {notesForFolder.map(note => (
+          {getNotesForFolder(this.context.notes, folderId).map(note => (
             <li key={note.id}>
-              <Note id={note.id} name={note.name} modified={note.modified} />
+              <Note
+                handleDelete={this.context.handleDelete}
+                id={note.id}
+                name={note.name}
+                modified={note.modified}
+              />
             </li>
           ))}
         </ul>
@@ -44,3 +42,10 @@ export default class NoteListMain extends React.Component {
     );
   }
 }
+
+NoteListMain.defaultProps = {
+  notes: [],
+};
+NoteListMain.propTypes = {
+  folderId: PropTypes.string,
+};
